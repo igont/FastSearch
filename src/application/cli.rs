1304508453a2bf949f904672427a04baec911e77
IgorWork <igorgontaren@mail.ru>
2026-08-13 15:02:58 +0300

    C2-REFACTOR apply workspace formatter

--- src/application/cli.rs
+++ src/application/cli.rs
@@ -7,7 +7,7 @@ use crate::{
     },
     ports::AgentSurface,
 };
-use serde_json::{json, Value};
+use serde_json::{Value, json};
 
 const USAGE: &str = "usage:\n  fastsearch init <documents> <code> <service> [e5-root]\n  fastsearch index update <documents> <code> <service> [e5-root]\n  fastsearch index rebuild <documents> <code> <service> [e5-root]\n  fastsearch search <documents> <code> <service> <balanced|current|design> <query> [e5-root]\n  fastsearch get <documents> <code> <service> <stable-id> [e5-root]\n  fastsearch related <documents> <code> <service> <stable-id> [e5-root]\n  fastsearch status <documents> <code> <service> [e5-root]";
 
@@ -797,7 +797,7 @@ mod presenters {
 
 #[cfg(test)]
 mod command_contract_tests {
-    use super::{parse_command, render_outcome, CommandOutcome, OutputFormat};
+    use super::{CommandOutcome, OutputFormat, parse_command, render_outcome};
 
     #[test]
     fn parses_a_production_status_command_once_before_dispatch() {
